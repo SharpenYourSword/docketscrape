ae7ca501c80797d3d61b3c1c486309047bbdf1c1
joegusz <joegusz@gmail.com>
2015-05-01 10:47:32 -0500

    Update .py

--- .py
+++ .py
@@ -1,13 +1,25 @@
 from mechanize import Browser  
-import urllib2
+import urllib2, csv, os
 from BeautifulSoup import BeautifulSoup
+from apscheduler.scheduler import Scheduler
 
-data = open('Docket.html')
+sched = Scheduler()
+sched.start()
 
-soup = BeautifulSoup(data)
+# data = open('Docket.html')
+
+html = urllib2.urlopen('https://www.courts.mo.gov/casenet/cases/searchDockets.do?inputVO.caseNumber=14BA-CV01093&inputVO.courtId=CT13').read()
+
+# br = Browser()
+# br.open(url)
+
+soup = BeautifulSoup(html)
 results_table = soup.find('table', attrs={'class': 'detailRecordTable'})
 
 output = []
+
+if os.path.isfile('current.csv'):
+	os.rename('current.csv', 'previous.csv')
 
 for tr in results_table.findAll('tr'):
 	output_row = []
@@ -18,7 +30,23 @@ for tr in results_table.findAll('tr'):
 		output_row.append(data)
 		output.append(output_row)
 
-		print output
-		print len(output)
-		if len(output) > '107':
-			print 'There is a new entry today.'
+with open('current.csv', 'w') as csvfile:
+	writer = csv.writer(csvfile, delimiter='|')
+	writer.writerows(output)
+
+with open('current.csv', 'r') as csvfile:
+	reader = csv.reader(csvfile, delimiter='|')
+	cur_row_count = sum(1 for row in reader)
+	print 'Current count:'
+	print cur_row_count
+
+with open('previous.csv', 'r') as csvfile: 
+	reader = csv.reader(csvfile, delimiter='|')
+	prev_row_count = sum(1 for row in reader)
+	print "Previous count:"
+	print prev_row_count
+
+if cur_row_count > prev_row_count:
+	print 'Good morning. There is a new entry in case number 14BA-CV01093 today.'
+elif cur_row_count == prev_row_count:
+	print 'Good morning. There are no new entries in case number 14BA-CV01093 today.'
